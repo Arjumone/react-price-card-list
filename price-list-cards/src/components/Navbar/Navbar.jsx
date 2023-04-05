d5a6a3a8cbfb26bbffd83e8815c928e5c4fb7d3d
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-04-05 15:19:47 +0600

    added price card

---
+++ price-list-cards/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Link from '../Link/Link';
+import { Bars3Icon,XMarkIcon } from '@heroicons/react/24/solid';
+
+const Navbar = () => {
+const [open,setOpen] = useState(false);
+
+    const routes = [
+        { id: 1, name: 'Home', path: '/' },
+        { id: 2, name: 'Blog', path: '/blog' },
+        { id: 3, name: 'Products', path: '/products' },
+        { id: 4, name: 'About Us', path: '/about-us' },
+        { id: 5, name: 'Contact Us', path: '/contact-us' }
+      ];
+      
+    return (
+        <nav className='bd-purple-400'>
+          <div className='md:hidden' onClick={() => setOpen(!open)}>
+            <span>
+                {open === true?
+             <XMarkIcon className="h-6 w-6 text-purple-500" />:
+              <Bars3Icon className="h-6 w-6 text-purple-500" />
+              }
+              </span>
+          </div>
+            <ul className={`md:flex absolute md:static bg-purple-400 pl-8 pb-4 py-2 duration-500 ${open ? 'top-6':'-top-36'}`}>
+               {
+                 routes.map( route => <Link key={route.id} route={route}></Link>)
+               }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ price-list-cards/src/components/PriceList/PriceList.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import PriceCard from '../PriceCard/PriceCard';
+
+const PriceList = () => {
+const [prices,setPrices] = useState([]);
+useEffect(() =>{
+    fetch('prices.json')
+    .then(res => res.json())
+    .then(data => setPrices(data));
+},[])
+    return (
+        <div>
+            <h3 className='text-6xl bg-purple-300 font-bold text-center text-purple-950  p-5'>Awosome affordable price</h3>
+           <div className='grid grid-cols-3 gap-3'>
+           {
+                prices.map(price => <PriceCard key={price.id} price={price}></PriceCard>)
+            }
+           </div>
+        </div>
+    );
+};
+
+export default PriceList;
